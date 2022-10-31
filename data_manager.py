029f0d97e3de2daf4578ef20a223448f137258e2
misterelgo <misterelgo@gmail.com>
2022-10-31 13:27:42 +0100

    succesfully requested the Sheety API

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,15 @@
+import requests
+from pprint import pprint
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.projectName = "flightDeals"
+        self.sheetName = "prices"
+        self.sheety_API_URL = "https://api.sheety.co/10fce03195300cfd5c84ea323144da6f/" + self.projectName + "/" + self.sheetName
+
+    def get_data(self):
+        sheety_response = requests.get(url=self.sheety_API_URL)
+        sheety_response.raise_for_status()
+        sheety_result = sheety_response.json()
+        return sheety_result
+
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
+
+my_data_manager = DataManager()
+sheet_data = my_data_manager.get_data()
+
+city_info = sheet_data["prices"]
+for city in city_info:
+    if city['iataCode'] == '':
+        print("its empty")
